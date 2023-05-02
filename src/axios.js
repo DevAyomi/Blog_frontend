6ae53e54218b0a6f7a40c2e81a6baa68530e8096
Devayo <gbolagadewinner@gmail.com>
2023-05-02 03:49:53 -0700

    Updates made for deployment

--- src/axios.js
+++ src/axios.js
@@ -7,32 +7,35 @@ import { useMainStore } from "@/stores/main.js";
 import router from "@/router";
 
 const axiosClient = axios.create({
-    baseURL: "http://127.0.0.1:8000/api"
-})
+  baseURL: "https://investigationdigest.com/api/api/",
+});
 
-axiosClient.interceptors.request.use(config => {
-  config.headers.Authorization = `Bearer ${ useMainStore().token }`
+axiosClient.interceptors.request.use((config) => {
+  config.headers.Authorization = `Bearer ${useMainStore().token}`;
   return config;
-})
+});
 
-axiosClient.interceptors.response.use(response => {
-  return response;
-}, error => {
-  if (error.response.status === 401) {
-   useMainStore().setUser({
-      name: "",
-      email: "",
-      token: null,
-      avatar: "",
-    });
-    sessionStorage.removeItem("TOKEN");
-    sessionStorage.removeItem("userType");
-    router.push('/');
-    location.reload()
-  } else if (error.response.status === 404) {
-    router.push({name: 'NotFound'})
+axiosClient.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response.status === 401) {
+      useMainStore().setUser({
+        name: "",
+        email: "",
+        token: null,
+        avatar: "",
+      });
+      sessionStorage.removeItem("TOKEN");
+      sessionStorage.removeItem("userType");
+      router.push("/");
+      location.reload();
+    } else if (error.response.status === 404) {
+      router.push({ name: "NotFound" });
+    }
+    throw error;
   }
-  throw error;
-})
+);
 
 export default axiosClient;
